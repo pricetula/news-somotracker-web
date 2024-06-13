892657db8f7fc376d7ea5663d4720e4de511dbff
Price Tula <pricetula@gmail.com>
2024-06-13 19:48:10 +0300

    start

---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+import Dashboard from "@/components/page/Dashboard/Dashboard";
+
+
+export default function Home() {
+  return <Dashboard />;
+}
---
+++ src/components/page/Dashboard/Dashboard.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+export default function Dashboard() {
+    return (
+        <main>
+            <h1>Example text</h1>
+            <Button>Click me</Button>
+        </main>
+    );
+}
